09577b0eb9de6668c0962fef6aef5e00b6a58b08
DEV-NOTE-07\makita <makita@j-yado.com>
2020-12-09 10:18:43 +0900

    add: フォーム送信とリダイレクト

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Product;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -41,5 +44,53 @@ class DefaultController extends Controller
             )
         );
     }
+
+    /**
+     * @Route("/form", name="formpage")
+     */
+    public function formAction(Request $request)
+    {
+        $product = new Product();
+        $product->setName('name1');
+        $product->setPrice(100);
+        $product->setDescription('description1');
+
+        $form = $this->createFormBuilder($product)
+            ->add('name', TextType::class)
+            ->add('price', NumberType::class)
+            ->add('description', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Create Product'))
+            ->getForm();
+
+        /**
+         * ユーザーがフォームを送信すると、Product の各プロパティにデータを書き込む。
+         */
+        $form->handleRequest($request);
+
+        /**
+         * isSubmitted() はフォームが送信されたかどうか
+         * isValid() は送信前は false フォーム送信時に検証した結果により変わる
+         */
+        if ($form->isSubmitted() && $form->isValid()) {
+            // フォーム送信成功時の処理
+
+
+            return $this->redirectToRoute('submitted');
+        }
+
+        return $this->render(
+            'default/form.html.twig',
+            array(
+                'form' => $form->createView(),
+            )
+        );
+    }
+
+    /**
+     * @Route("/success", name="submitted")
+     */
+    public function submittedAction(Request $request)
+    {
+        return $this->render('default/submitted.html.twig');
     }
 }
